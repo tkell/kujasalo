89ad26fac5f6293e332c54ed28a424de74e879de
Thor Kell <thor@tide-pool.ca>
2019-01-31 20:34:09 -0500

    drawing both X and cross

--- red-and-red.rb
+++ red-and-red.rb
@@ -13,10 +13,10 @@ LINE_WIDTH = 6
 COLOR_ONE = 'red'
 COLOR_TWO = 'tomato'
 COLOR_THREE = 'gold'
+COLOR_FOUR = 'dark red' # !!
 
-f = Image.new(100,100) { self.background_color = "white" }
 canvas = Magick::ImageList.new
-canvas.new_image(250, 250) {self.background_color = "white"}
+canvas.new_image(450, 1250) {self.background_color = "white"}
 
 def draw_line(start_x, start_y, end_x, end_y, color)
   line = Magick::Draw.new
@@ -34,14 +34,19 @@ def draw_x(canvas, start_x, start_y, size, color_one, color_two)
   draw_line(start_x + size - 2, start_y + 2, start_x + 2, start_y + size - 2, color_two).draw(canvas)
 end
 
-def draw_row_with_x(canvas, size, row)
+def draw_cross(canvas, start_x, start_y, size, color_one, color_two)
+  draw_line(start_x, start_y + (size / 2), start_x + size, start_y + (size / 2), color_one).draw(canvas)
+  draw_line(start_x + (size / 2), start_y, start_x + (size / 2), start_y + size, color_two).draw(canvas)
+end
+
+def draw_row_with_x(canvas, size, row, row_offset)
   if row % 2 == 0
-    line_array = [0, 1, 0, 1, 0]
+    line_array = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
   else
-    line_array = [1, 0, 1, 0, 1]
+    line_array = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
   end
 
-  y_offset = size * row
+  y_offset = size * (row + row_offset)
   line_array.each_with_index do |item, index|
     x_offset = index * size
     if item == 0
@@ -57,8 +62,34 @@ def draw_row_with_x(canvas, size, row)
 end
 
 
-for row in 0..4 do
-  draw_row_with_x(canvas, 50, row)
+def draw_row_with_cross(canvas, size, row, row_offset)
+  if row % 2 == 0
+    line_array = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+  else
+    line_array = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+  end
+
+  y_offset = size * (row + row_offset)
+  line_array.each_with_index do |item, index|
+    x_offset = index * size
+    if item == 0
+      color_one = 'red'
+      color_two = 'tomato'
+    end
+    if item == 1
+      color_one = 'tomato'
+      color_two = 'gold'
+    end
+    draw_cross(canvas, x_offset,y_offset, size, color_one, color_two)
+  end
+end
+
+for row in 0..8 do
+  draw_row_with_x(canvas, 50, row, 0)
+end
+
+for row in 0..8 do
+  draw_row_with_cross(canvas, 50, row, 16)
 end
 
 canvas.write("output/test.jpg")
