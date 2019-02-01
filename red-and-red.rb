be4436d0d5346a34656d3945dd34f432c0abb858
Thor Kell <thor@tide-pool.ca>
2019-01-31 22:01:33 -0500

    working

--- red-and-red.rb
+++ red-and-red.rb
@@ -34,6 +34,34 @@ def draw_x(canvas, start_x, start_y, size, color_one, color_two)
   draw_line(start_x + size - 2, start_y + 2, start_x + 2, start_y + size - 2, color_two).draw(canvas)
 end
 
+def draw_x_with_counterclockwise_rotation(canvas, start_x, start_y, size, color_one, color_two, rotation_offset)
+  draw_line(start_x,
+            start_y + rotation_offset,
+            start_x + size,
+            start_y + size - rotation_offset,
+            color_one).draw(canvas)
+
+  draw_line(start_x + size - rotation_offset,
+           start_y,
+           start_x + rotation_offset,
+           start_y + size,
+           color_two).draw(canvas)
+end
+
+def draw_x_with_clockwise_rotation(canvas, start_x, start_y, size, color_one, color_two, rotation_offset)
+  draw_line(start_x + rotation_offset,
+            start_y,
+            start_x + size - rotation_offset,
+            start_y + size,
+            color_one).draw(canvas)
+
+  draw_line(start_x + size,
+           start_y + rotation_offset,
+           start_x,
+           start_y + size - rotation_offset,
+           color_two).draw(canvas)
+end
+
 def draw_cross(canvas, start_x, start_y, size, color_one, color_two)
   draw_line(start_x, start_y + (size / 2), start_x + size, start_y + (size / 2), color_one).draw(canvas)
   draw_line(start_x + (size / 2), start_y, start_x + (size / 2), start_y + size, color_two).draw(canvas)
@@ -61,6 +89,29 @@ def draw_row_with_x(canvas, size, row, row_offset)
   end
 end
 
+def draw_row_with_rotation(canvas, size, row, row_offset, rotation_offset)
+  if row % 2 == 0
+    line_array = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+  else
+    line_array = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+  end
+
+  y_offset = size * (row + row_offset)
+  line_array.each_with_index do |item, index|
+    x_offset = index * size
+    if item == 0
+      color_one = 'red'
+      color_two = 'tomato'
+      draw_x_with_counterclockwise_rotation(canvas, x_offset,y_offset, 50, color_one, color_two, rotation_offset)
+    end
+    if item == 1
+      color_one = 'tomato'
+      color_two = 'gold'
+      draw_x_with_clockwise_rotation(canvas, x_offset,y_offset, 50, color_one, color_two, rotation_offset)
+    end
+  end
+end
+
 
 def draw_row_with_cross(canvas, size, row, row_offset)
   if row % 2 == 0
@@ -84,9 +135,22 @@ def draw_row_with_cross(canvas, size, row, row_offset)
   end
 end
 
+
+
+## do it!
+
 for row in 0..8 do
   draw_row_with_x(canvas, 50, row, 0)
 end
+
+draw_row_with_rotation(canvas, 50, 9, 0, 3.125)
+draw_row_with_rotation(canvas, 50, 10, 0, 6.25)
+draw_row_with_rotation(canvas, 50, 11, 0, 9.375)
+draw_row_with_rotation(canvas, 50, 12, 0, 12.5)
+draw_row_with_rotation(canvas, 50, 13, 0, 15.625)
+draw_row_with_rotation(canvas, 50, 14, 0, 18.75)
+draw_row_with_rotation(canvas, 50, 15, 0, 21.875)
+
 
 for row in 0..8 do
   draw_row_with_cross(canvas, 50, row, 16)
